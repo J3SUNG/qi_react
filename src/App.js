90681faeff15a1cf93ef02de8e628351e5fba1ab
J3SUNG <wptjd6141@naver.com>
2021-06-04 02:52:41 -0700

    react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { BrowserRouter, Link } from "react-router-dom";
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <BrowserRouter>
+      <div>
+        <Link to="/Tableau" className="content">
+          Tableau
+        </Link>
+        <Link to="/Bokeh" className="content">
+          Bokeh
+        </Link>
+        <Link to="/Zeppelin" className="content">
+          Zeppelin
+        </Link>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
