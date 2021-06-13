4e3599770074789f071239aba2adf52fcc994497
J3SUNG <wptjd6141@naver.com>
2021-06-12 22:17:58 -0700

    bokeh

--- src/PrintImg.js
+++ src/PrintImg.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./PrintImg.scss";
 import tableau_map from "./images/tableau_map.jpg";
+import bokeh_plot from "./images/bokeh_plot.png";
 const PrintImg = ({ check }) => {
   console.log(check);
   return (
     <div className="imgBox">
       {check.tab ? (
         <img src={tableau_map} alt="tableau_map" className="img" />
+      ) : undefined}
+      {check.bok ? (
+        <img src={bokeh_plot} alt="bokeh_plot" className="img" />
       ) : undefined}
     </div>
   );
