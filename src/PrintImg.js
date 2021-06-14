eff60d057f099418edf6f89b9b1e9665a33d7a41
J3SUNG <wptjd6141@naver.com>
2021-06-13 23:26:14 -0700

    zep

--- src/PrintImg.js
+++ src/PrintImg.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./PrintImg.scss";
 import tableau_map from "./images/tableau_map.jpg";
 import bokeh_plot from "./images/bokeh_plot.png";
+import zeppelin from "http://localhost:8989/#/notebook/2GAQ518C5/paragraph/20210613-193528_827427093?asIframe";
+
 const PrintImg = ({ check }) => {
   console.log(check);
   return (
@@ -11,6 +13,9 @@ const PrintImg = ({ check }) => {
       ) : undefined}
       {check.bok ? (
         <img src={bokeh_plot} alt="bokeh_plot" className="img" />
+      ) : undefined}
+      {check.zep ? (
+        <img src={zeppelin} alt="zeppelin" className="img" />
       ) : undefined}
     </div>
   );
