34456d1c52b63d66fb6e2e7881cb599995eceb6f
J3SUNG <wptjd6141@naver.com>
2021-06-13 23:57:53 -0700

    ifream

--- src/PrintImg.js
+++ src/PrintImg.js
@@ -15,7 +15,7 @@ const PrintImg = ({ check }) => {
       ) : undefined}
       {check.zep ? (
         <iframe
-          src="https://www.youtube.com/embed/cWDJoK8zw58"
+          src="http://localhost:8989/#/notebook/2GAQ518C5/paragraph/20210613-193528_827427093?asIframe"
           className="img"
         />
       ) : undefined}
