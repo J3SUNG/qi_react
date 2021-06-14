369670ca544b0a05956554f63f66df8d01e0fb51
J3SUNG <wptjd6141@naver.com>
2021-06-13 23:43:17 -0700

    test

--- src/PrintImg.js
+++ src/PrintImg.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./PrintImg.scss";
 import tableau_map from "./images/tableau_map.jpg";
 import bokeh_plot from "./images/bokeh_plot.png";
-import zeppelin from "https://pbs.twimg.com/profile_images/770139154898382848/ndFg-IDH.jpg";
+import zeppelin from "https://www.solarwinds.com/-/media/solarwinds/swdcv2/licensed-products/service-desk/integrations/sd-integrations-logo-google-single-sign-on.ashx?rev=e5d31b9fa12848d3bd44c8c960d0dd3b&hash=7BD65F7FD1EF91A9248E1C4167CB01E4";
 
 const PrintImg = ({ check }) => {
   console.log(check);
