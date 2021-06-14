7de726f4b8dc1e7aeb6d53144dc4e012fc072e36
J3SUNG <wptjd6141@naver.com>
2021-06-13 23:58:22 -0700

    test

--- src/PrintImg.js
+++ src/PrintImg.js
@@ -14,10 +14,7 @@ const PrintImg = ({ check }) => {
         <img src={bokeh_plot} alt="bokeh_plot" className="img" />
       ) : undefined}
       {check.zep ? (
-        <iframe
-          src="http://localhost:8989/#/notebook/2GAQ518C5/paragraph/20210613-193528_827427093?asIframe"
-          className="img"
-        />
+        <iframe src="https://github.com/gogumaa/zeppelin-web" className="img" />
       ) : undefined}
     </div>
   );
