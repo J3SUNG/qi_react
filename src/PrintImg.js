571f90a8ac5b028b26d4c0e2c1f6bd46de85f6ff
J3SUNG <wptjd6141@naver.com>
2021-06-14 00:58:39 -0700

    test

--- src/PrintImg.js
+++ src/PrintImg.js
@@ -13,6 +13,11 @@ const PrintImg = ({ check }) => {
       {check.bok ? (
         <img src={bokeh_plot} alt="bokeh_plot" className="img" />
       ) : undefined}
+      <iframe
+        src="http://localhost:8989/#/notebook/2GAQ518C5/paragraph/20210613-193528_827427093?asIframe"
+        height="500px"
+        width="500px"
+      ></iframe>
       {/* {check.zep ? (
         <iframe
           src={zeppelin_html}
