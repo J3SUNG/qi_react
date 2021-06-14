c600d690abaf603df988628d4d0273cbc13d33da
J3SUNG <wptjd6141@naver.com>
2021-06-13 23:46:03 -0700

    test

--- src/PrintImg.js
+++ src/PrintImg.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./PrintImg.scss";
 import tableau_map from "./images/tableau_map.jpg";
 import bokeh_plot from "./images/bokeh_plot.png";
-import zeppelin from "https://www.solarwinds.com/-/media/solarwinds/swdcv2/licensed-products/service-desk/integrations/sd-integrations-logo-google-single-sign-on.ashx?rev=e5d31b9fa12848d3bd44c8c960d0dd3b&hash=7BD65F7FD1EF91A9248E1C4167CB01E4";
 
 const PrintImg = ({ check }) => {
   console.log(check);
@@ -15,7 +14,11 @@ const PrintImg = ({ check }) => {
         <img src={bokeh_plot} alt="bokeh_plot" className="img" />
       ) : undefined}
       {check.zep ? (
-        <img src={zeppelin} alt="zeppelin" className="img" />
+        <img
+          src="https://thisinterestsme.com/LOGO.png?x75840"
+          alt="zeppelin"
+          className="img"
+        />
       ) : undefined}
     </div>
   );
